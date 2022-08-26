51e9ad699f2f8a6825f33245e635d1b848f2a129
fansters <edmundseizentals@gmail.com>
2022-08-26 20:29:50 +0300

    working on hero section, deepdive into tailwind docs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,13 +7,13 @@ const Navbar = () => {
 	const [toggle, setToggle] = useState(false);
 	return (
 		<nav className=' w-full flex py-6 justify-between items-center navbar '>
-			<img src={logo} alt='finance' className='w-[124px] h-[32px]' />
+			<img src={logo} alt='finance' className='w-[7.75rem] h-[2rem]' />
 
 			<ul className='list-none sm:flex hidden justify-end items-center flex-1'>
 				{navLinks.map((nav, index) => (
 					<li
 						key={nav.id}
-						className={`font-poppins font-normal cursor-pointer text-[16px] ${
+						className={`font-poppins font-normal cursor-pointer text-[1rem] ${
 							index === navLinks.length - 1 ? `mr-0` : `mr-10`
 						} text-white `}
 					>
@@ -25,19 +25,19 @@ const Navbar = () => {
 				<img
 					src={toggle ? close : menu}
 					alt='menu'
-					className='w-[28px] h-[28px] object-contain'
+					className='w-[1.75rem] h-[1.75rem] object-contain'
 					onClick={() => setToggle((prev) => !prev)}
 				/>
 				<div
 					className={`${
 						!toggle ? "hidden" : "flex"
-					} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-4 min-w-[140px] rounded-xl sidebar`}
+					} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-4 min-w-[8.75rem] rounded-xl sidebar`}
 				>
 					<ul className='list-none flex flex-col justify-end items-center flex-1'>
 						{navLinks.map((nav, index) => (
 							<li
 								key={nav.id}
-								className={`font-poppins font-normal cursor-pointer text-[16px] ${
+								className={`font-poppins font-normal cursor-pointer text-[1rem] ${
 									index === navLinks.length - 1 ? `mr-0` : `mb-4`
 								} text-white `}
 							>
