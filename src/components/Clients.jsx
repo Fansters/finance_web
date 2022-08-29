6a782cbcf526ac26119ba5f52ea2a2529a683d5c
fansters <edmundseizentals@gmail.com>
2022-08-29 14:18:16 +0300

    added clients and CTA button

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { clients } from "../constants";
+import styles from "../style";
 
-const Clients = () => {
-  return (
-    <div>Clients</div>
-  )
-}
-
-export default Clients
+const Clients = () => (
+	<section className={`${styles.flexCenter} my-4`}>
+		<div className={`${styles.flexCenter} flex-wrap w-full`}>
+			{clients.map((client) => (
+				<div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[12rem] min-w-[7.5rem]`}>
+					<img src={client.logo} alt='client' className='sm:w-[12rem] w-[6.25rem] object-contain ' />
+				</div>
+         ))}
+		</div>
+	</section>
+);
+export default Clients;
